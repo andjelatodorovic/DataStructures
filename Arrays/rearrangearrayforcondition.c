b78fa1e8c38bb0c436bd085a6aacb188222378f2
Anđela Todorović <andjelatodorovicc@gmail.com>
2019-06-11 23:41:45 +0200

    Add files via upload

---
+++ Arrays/rearrangearrayforcondition.c
@@ -0,0 +1,57 @@
+// C++ program to rearrange the array 
+// as per the given condition 
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// function to rearrange the array 
+void rearrangeArr(int arr[], int n) 
+{ 
+	// total even positions 
+	int evenPos = n / 2; 
+
+	// total odd positions 
+	int oddPos = n - evenPos; 
+
+	int tempArr[n]; 
+
+	// copy original array in an 
+	// auxiliary array 
+	for (int i = 0; i < n; i++) 
+		tempArr[i] = arr[i]; 
+
+	// sort the auxiliary array 
+	sort(tempArr, tempArr + n); 
+
+	int j = oddPos - 1; 
+
+	// fill up odd position in original 
+	// array 
+	for (int i = 0; i < n; i += 2) { 
+		arr[i] = tempArr[j]; 
+		j--; 
+	} 
+
+	j = oddPos; 
+
+	// fill up even positions in original 
+	// array 
+	for (int i = 1; i < n; i += 2) { 
+		arr[i] = tempArr[j]; 
+		j++; 
+	} 
+
+	// display array 
+	for (int i = 0; i < n; i++) 
+		cout << arr[i] << " "; 
+} 
+
+// Driver code 
+int main() 
+{ 
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7 }; 
+	int size = sizeof(arr) / sizeof(arr[0]); 
+	rearrangeArr(arr, size); 
+	return 0; 
+} 
+
